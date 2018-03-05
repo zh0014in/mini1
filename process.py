3485c54830b802bd08bd6adc2702b362a3674dae
Zhang Mingmin <zh0014in@e.ntu.edu.sg>
2018-03-05 16:44:20 +0800

    vis2,3

--- process.py
+++ process.py
@@ -3,6 +3,7 @@ import datetime
 import csv
 from pprint import pprint
 
+#first vis
 data = json.load(open('participation.json'))
 
 result = [data["owner"], [x-y for x,y in zip(data["all"], data["owner"])]]
@@ -12,6 +13,20 @@ with open('participation.csv','w') as file:
     wr.writerow(['owner', 'others'])
     for x in reversed(range(0, len(result[0]))):
         wr.writerow([result[0][x], result[1][x]])
+
+#second vis
+data = json.load(open('workinghour.json'))
+with open('workinghour.csv', 'w') as file:
+    wr = csv.writer(file)
+    wr.writerow(['sunday', 'monday', 'tuesday', 'wendsday', 'thirsday', 'friday', 'saturday'])
+    for x in range(8, 19):
+        hour = []
+        for y in [a for a in data if a[1] == x]:
+            hour.append(y[2])
+        wr.writerow(hour)
+
+
+
 
 # owner =  [a for a in data if a["author"]["login"]=="torvalds"]
 # pprint(owner)
