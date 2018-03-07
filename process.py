0f9ad1b6ba4d5bf331b8d97135cf39b08181c86b
Zhang Mingmin <zh0014in@e.ntu.edu.sg>
2018-03-07 21:44:11 +0800

    add function to change repos

--- process.py
+++ process.py
@@ -3,6 +3,8 @@ import datetime
 import csv
 import requests
 import os.path
+import operator
+import sys
 
 
 def writeJsonToFile(path, data):
@@ -10,13 +12,22 @@ def writeJsonToFile(path, data):
         json.dump(data, outfile)
 
 
+user = 'torvalds'
+repo = 'linux'
+if len(sys.argv) == 3:
+    user = sys.argv[1]
+    repo = sys.argv[2]
+elif len(sys.argv) != 1:
+    print 'please provide both user and repo or leave them empty'
+    exit
+
 # first vis
-if os.path.isfile('json/participation.json'):
-    data = json.load(open('json/participation.json'))
+if os.path.isfile('json/'+user+'_'+repo+'_participation.json'):
+    data = json.load(open('json/'+user+'_'+repo+'_participation.json'))
 else:
     data = requests.get(
-        "https://api.github.com/repos/torvalds/linux/stats/participation").json()
-    writeJsonToFile('json/participation.json', data)
+        "https://api.github.com/repos/"+user+"/"+repo+"/stats/participation").json()
+    writeJsonToFile('json/'+user+'_'+repo+'_participation.json', data)
 result = [data["owner"], [x-y for x, y in zip(data["all"], data["owner"])]]
 with open('participation.csv', 'w') as file:
     wr = csv.writer(file, lineterminator='\n')
@@ -25,12 +36,12 @@ with open('participation.csv', 'w') as file:
         wr.writerow([result[0][x], result[1][x]])
 
 # second vis
-if os.path.isfile('json/workinghour.json'):
-    data = json.load(open('json/workinghour.json'))
+if os.path.isfile('json/'+user+'_'+repo+'_workinghour.json'):
+    data = json.load(open('json/'+user+'_'+repo+'_workinghour.json'))
 else:
     data = requests.get(
-        "https://api.github.com/repos/torvalds/linux/stats/punch_card").json()
-    writeJsonToFile('json/workinghour.json', data)
+        "https://api.github.com/repos/"+user+"/"+repo+"/stats/punch_card").json()
+    writeJsonToFile('json/'+user+'_'+repo+'_workinghour.json', data)
 with open('workinghour.csv', 'w') as file:
     wr = csv.writer(file, lineterminator='\n')
     wr.writerow(['sunday', 'monday', 'tuesday', 'wendnesday',
@@ -42,7 +53,7 @@ with open('workinghour.csv', 'w') as file:
         wr.writerow(hour)
 
 # third vis
-languagesBytesCandOthers = {"C": 0, "Others": 0}
+languagesBytesTopandOthers = {"Others": 0}
 languagesBytesOthers = {}
 languagesBytesAll = {}
 
@@ -55,47 +66,44 @@ def mergeToLanguagesBytes(input):
             languagesBytesAll[propertyName] = 0
             languagesBytesAll[propertyName] += input[propertyName]
 
-    for propertyName in input:
-        if propertyName == 'C':
-            languagesBytesCandOthers["C"] += input[propertyName]
-        else:
-            languagesBytesCandOthers["Others"] += input[propertyName]
-    for propertyName in input:
-        if propertyName == 'C':
-            continue
-        if propertyName in languagesBytesOthers:
-            languagesBytesOthers[propertyName] += input[propertyName]
-        else:
-            languagesBytesOthers[propertyName] = 0
-            languagesBytesOthers[propertyName] += input[propertyName]
+
+def split():
+    largest = max(languagesBytesAll.iteritems(), key=operator.itemgetter(1))[0]
+    languagesBytesTopandOthers[largest] = languagesBytesAll[largest]
+    for x in languagesBytesAll:
+        if x != largest:
+            languagesBytesTopandOthers["Others"] += languagesBytesAll[x]
+            languagesBytesOthers[x] = languagesBytesAll[x]
 
 
 def getLanguages(repos, index):
     if(index >= len(repos)):
+        split()
         writeJsonToFile('languages.json', languagesBytesOthers)
-        writeJsonToFile('languagesC.json', languagesBytesCandOthers)
+        writeJsonToFile('languagesTopAndOthers.json',
+                        languagesBytesTopandOthers)
         with open('languages.csv', 'w') as file:
             wr = csv.writer(file, lineterminator='\n')
             wr.writerow(['language', 'bytes'])
-            # print(languagesBytesAll)
             for x in languagesBytesAll:
                 wr.writerow([x, languagesBytesAll[x]])
         return
-    if os.path.isfile('json/'+repos[index]["name"]+'.json'):
-        languages = json.load(open('json/'+repos[index]["name"]+'.json'))
+    if os.path.isfile('json/'+user+'_'+repo+'_'+repos[index]["name"]+'.json'):
+        languages = json.load(
+            open('json/'+user+'_'+repo+'_'+repos[index]["name"]+'.json'))
     else:
         languages = requests.get(
-            "https://api.github.com/repos/torvalds/" + repos[index]["name"] + "/languages").json()
-        writeJsonToFile('json/'+repos[index]["name"]+'.json', languages)
-    # print(repos[index]["name"])
+            "https://api.github.com/repos/"+user+"/" + repos[index]["name"] + "/languages").json()
+        writeJsonToFile('json/'+user+'_'+repo+'_' +
+                        repos[index]["name"]+'.json', languages)
     mergeToLanguagesBytes(languages)
     getLanguages(repos, index+1)
     return
 
 
-if os.path.isfile('json/repos.json'):
-    repos = json.load(open('json/repos.json'))
+if os.path.isfile('json/'+user+'_'+repo+'_repos.json'):
+    repos = json.load(open('json/'+user+'_'+repo+'_repos.json'))
 else:
-    repos = requests.get("https://api.github.com/users/torvalds/repos").json()
-    writeJsonToFile('json/repos.json', repos)
+    repos = requests.get("https://api.github.com/users/"+user+"/repos").json()
+    writeJsonToFile('json/'+user+'_'+repo+'_repos.json', repos)
 getLanguages(repos, 0)
